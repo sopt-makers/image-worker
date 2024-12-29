683384e2f6ce28fdfe37b7cab3f971d88a8e029a
borimong <kimhyunsu11@naver.com>
2024-12-29 21:44:55 +0900

    feat: poc 완료

--- workers/image-proxy-worker/src/index.ts
+++ workers/image-proxy-worker/src/index.ts
@@ -17,16 +17,21 @@ export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url);
 
-		const width = url.searchParams.get('width');
+		const widthStr = url.searchParams.get('width');
 		const imageUrl = url.searchParams.get('url');
 
 		if (imageUrl == null) {
 			return new Response('에러에러 url 이 널', { status: 400 });
 		}
 
-		if (width == null) {
+		if (widthStr == null || isNaN(Number(widthStr))) {
 			return new Response('에러에러 width가 널', { status: 400 });
 		}
+
+		const width = Number(widthStr);
+
+		// TODO: url이 메이커스 이미지 URL만 허용하도록
+		// 다양한 이미지 확정자를 다 받을 수 있는지, 큰 이미지 받을 수 있는지, 못 받으면 cpu 시간 높이기, 캐시 잘 되는지 로그, 브라우저 캐시 클플 요청 자체를 브라우저 캐시가 줄일 수 있음, 도메인, 커스텀 로더, 브라우저별로도,
 
 		//cache
 		const cacheKey = new Request(url.toString());
@@ -35,11 +40,11 @@ export default {
 		let response = await cache.match(cacheKey);
 
 		if (response == null) {
-			console.log(`Response for request url: ${request.url} not present in cache. Fetching and caching request.`);
+			console.log(`Cache Miss: width=${width}, url=${url}`);
 
 			const imageData = await fetch(imageUrl);
 
-			const resizedImage = await resizeImage(await imageData.arrayBuffer());
+			const resizedImage = await resizeImage(await imageData.arrayBuffer(), width);
 
 			response = new Response(resizedImage, {
 				headers: {
@@ -51,7 +56,7 @@ export default {
 
 			ctx.waitUntil(cache.put(cacheKey, response.clone()));
 		} else {
-			console.log(`Cache hit for: ${request.url}.`);
+			console.log(`Cache Hit: width=${width}, url=${url}`);
 		}
 
 		return response;
--- workers/image-proxy-worker/src/resizer.ts
+++ workers/image-proxy-worker/src/resizer.ts
@@ -1,10 +1,16 @@
-export const resizeImage = async (imageData: ArrayBuffer) => {
+export const resizeImage = async (imageData: ArrayBuffer, targetWidth: number) => {
 	const { PhotonImage, resize, SamplingFilter } = await import('@cf-wasm/photon');
 
 	const inputBytes = new Uint8Array(imageData);
 	const inputImage = PhotonImage.new_from_byteslice(inputBytes);
 
-	const outputImage = resize(inputImage, inputImage.get_width() * 0.2, inputImage.get_height() * 0.2, SamplingFilter.Nearest);
+	const imageWidth = inputImage.get_width();
+	const imageHeight = inputImage.get_height();
+
+	// imageWidth : imageHeight = targetWidth : targetHeight
+	const targetHeight = (imageHeight * targetWidth) / imageWidth;
+
+	const outputImage = resize(inputImage, targetWidth, targetHeight, SamplingFilter.Nearest);
 
 	const outputBytes = outputImage.get_bytes_webp();
 
