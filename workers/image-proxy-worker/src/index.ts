67eb70175bb3d5af3553f2b246c6932b3a520f5a
Hyeonsu Kim <kimhyunsu11@naver.com>
2025-02-11 16:55:48 +0900

    debug: whitelist 되살리기

--- workers/image-proxy-worker/src/index.ts
+++ workers/image-proxy-worker/src/index.ts
@@ -37,9 +37,9 @@ export default {
 
 		const isAllowed = whiteListUrl.some((allowedUrl) => imageUrl?.startsWith(allowedUrl));
 
-		// if (!isAllowed) {
-		// 	return new Response('허용되지 않은 이미지 url 입니다.', { status: 403 });
-		// }
+		if (!isAllowed) {
+			return new Response('허용되지 않은 이미지 url 입니다.', { status: 403 });
+		}
 		//원본 이미지
 		const unResizedImage = await fetch(imageUrl);
 
